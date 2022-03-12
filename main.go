ea0b04b4b47b35afe852e9d762d327fd246154e7
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-03-13 02:33:08 +0900

    get Request

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main()  {
-	fmt.Print("Hello World")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "こんにちは世界")
 }
